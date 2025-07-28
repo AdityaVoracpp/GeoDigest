832f82e96b1406469c29967d7a1f763b2fc4b2b9
AdityaVoracpp <adityavora9197@gmail.com>
2025-07-28 23:05:37 +0530

    Update server.js Added AI summary option

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 const fs = require('fs');
+const { GoogleGenAI }= require ("@google/genai");
+
 const Sentiment = require('sentiment');
 const sentiment = new Sentiment();
+
 
 const cities = JSON.parse(fs.readFileSync('./cities.json','utf-8'));
 const countries = JSON.parse(fs.readFileSync('./countries.json','utf-8'));
@@ -13,8 +16,21 @@ app.use(cors());                     //allows users from any server/ ip to acces
 app.use(express.json());
 
 
+const aiparts = ["AIzaSyA","6wS2QJX","9c5yImWZn","UHTzX0V","3oq3oUt0Q"]
+const aaky = aiparts.join("");
+const ai = new GoogleGenAI({ apiKey: aaky });
+
 const parts = ["KhOoLKTVC4r","Q4kj1c0U5","OYWQrGj06o","USZWzEfmwi"];   // to prevent bot from scraping API key
 const antibot = parts.join("");
+
+
+async function aisummarize(txt) {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: "Summarize and give some opinion/inference from the following news under 100 words: " + txt,
+  });
+  return response.text;
+}
 
 function getSentimentValue(text) {
   const result = sentiment.analyze(text);
@@ -25,7 +41,7 @@ function getSentimentValue(text) {
   return 0;
 }
 
-function findLocation(article, cities, countries) {
+async function findLocation(article, cities, countries) {
 
     if(article.title===''){
         article.title = 'undefined';
@@ -43,9 +59,12 @@ function findLocation(article, cities, countries) {
 
 
     const text = `${article.title} ${article.description} ${article.keywords} ${article.snippet}`;
-    console.log(text);
+    const airaw = `${article.title}  ${article.description}`;
+
     
     const senti = getSentimentValue(text);
+    const airesponse = await aisummarize(airaw);
+
 
     for (let i = 0; i < cities.length; i++) {
         const cityName = cities[i].city;
@@ -56,7 +75,8 @@ function findLocation(article, cities, countries) {
                 type: 'city',
                 lat: cities[i].latitude,
                 lon: cities[i].longitude,
-                senti: senti
+                senti: senti,
+                summary: airesponse
             };
         }
     } 
@@ -70,7 +90,8 @@ function findLocation(article, cities, countries) {
             type: 'country',
             lat: countries[i].latitude,
             lon: countries[i].longitude,
-            senti: senti
+            senti: senti,
+            summary: airesponse
             };
         }
     }
@@ -81,12 +102,10 @@ function findLocation(article, cities, countries) {
         type: 'none',
         lat: 0,
         lon: -160,
-        senti: senti
+        senti: senti,
+        summary: airesponse
     };
 }
-
-
-
 
 
 app.post('/news', async (req,res)=>{
@@ -104,26 +123,30 @@ app.post('/news', async (req,res)=>{
 
         const rawresarray = response.data.data;
 
-        const resarray = [];
-        for(let i=0;i<rawresarray.length;i++){
-            const location_data = findLocation(rawresarray[i],cities,countries);
-            const offset = (Math.random() - 0.5) * 0.1;
-            resarray.push({
-                title : rawresarray[i].title,
-                description : rawresarray[i].description,
-                keywords : rawresarray[i].keywords,
-                url: rawresarray[i].url,
-                image_url : rawresarray[i].image_url,
-                language : rawresarray[i].language,
-                source : rawresarray[i].source,
-                categories : rawresarray[i].categories,
-                location: location_data.match,
-                location_type: location_data.type,
-                lat: location_data.lat + offset,
-                lon : location_data.lon + offset,
-                senti : location_data.senti
-            })
-        }
+        const resarray = await Promise.all(
+  rawresarray.map(async (article) => {
+    const location_data = await findLocation(article, cities, countries);
+    const offset = (Math.random() - 0.5) * 0.1;
+
+        return {
+            title: article.title,
+            description: article.description,
+            keywords: article.keywords,
+            url: article.url,
+            image_url: article.image_url,
+            language: article.language,
+            source: article.source,
+            categories: article.categories,
+            location: location_data.match,
+            location_type: location_data.type,
+            lat: location_data.lat + offset,
+            lon: location_data.lon + offset,
+            senti: location_data.senti,
+            summary: location_data.summary
+    };
+  })
+);
+
         console.log('resarray is: ', resarray);
         res.json(resarray);
         
@@ -148,13 +171,6 @@ catch (err) {
     msg: `Sorry, we couldn't fetch articles due to an internal error.`
   });
 }
-
-
-
-
-
-
-
 
 
 });
